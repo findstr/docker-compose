9cb8919a861a9eab02fbc19c99a4eb0c06cce728
findstr <findstr@sina.com>
2018-12-25 16:43:23 +0800

    remove expose of redis port

--- backup.sh
+++ backup.sh
@@ -4,13 +4,13 @@ CERT="/etc/letsencrypt/"
 SQL="wordpress.sql"
 WP="home/wwwroot/blog.gotocoding.com/wp-content"
 REDIS="data/"
-BACKUP="$(pwd)/$(date +%Y-%m-%d)"
+BACKUP="/home/findstr/backup/$(date +%Y-%m-%d)"
 mkdir $BACKUP
 
 function backup()
 {
 	echo "Backup start ..."
-	sudo docker exec ${PREFIX}mysql_1 /usr/bin/mysqldump -u root --password=wp --databases wordpress> ${SQL}
+	sudo docker exec ${PREFIX}mysql_1 /usr/bin/mysqldump -u root --password=wp --databases wordpress> $BACKUP/$SQL
 	sudo docker run --rm --volumes-from ${PREFIX}nginx_1 -v $BACKUP:/backup busybox tar -C/ -czvf /backup/cert.tar.gz ${CERT}
 	sudo docker run --rm --volumes-from ${PREFIX}php_1 -v $BACKUP:/backup busybox tar -C/ -czvf /backup/wordpress.tar.gz  ${WP}
 	sudo docker run --rm --volumes-from ${PREFIX}redis_1 -v $BACKUP:/backup busybox tar -C/ -czvf /backup/redis.tar.gz ${REDIS}
